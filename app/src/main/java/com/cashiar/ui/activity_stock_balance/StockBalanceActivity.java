0e310773ee2f838a8fb31010d0c1ab7bf1fdc4be
filesdev7 <filesdev7@gmail.com>
2021-11-21 16:27:22 +0200

    emad

---
+++ app/src/main/java/com/cashiar/ui/activity_stock_balance/StockBalanceActivity.java
@@ -0,0 +1,16 @@
+package com.cashiar.ui.activity_stock_balance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cashiar.R;
+
+public class StockBalanceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock_balance);
+    }
+}
